9463d68e88aafbf6c5eb7e070b74fbcb01dc0241
AmiraAHady <amirasgithub@gmail.com>
2023-08-27 12:30:45 +0300

    angular day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+ //Decorator
+ //metadata
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'meanstackproject';
+  title = 'Netflix';
+  
+  sum(x: number, y: number) {
+    return x + y;
+  }
 }
